482593b56c83079051b2014aafe6da591fd02a42
hajjy22 <ismailhajjy02@gmail.com>
2024-12-11 02:14:59 +0100

    stm32 definitions

--- src/stm32f429_i2c_driver.c
+++ src/stm32f429_i2c_driver.c
@@ -1,10 +1,11 @@
-#include "stm32f407xx_i2c_driver.h"
+#include "stm32f429_i2c_driver.h"
 
 uint32_t RCC_GetPLLValue()
 {
-     return ;
+     return 0;
 }
 
+//To get PCLK1 APB frequency Value in CCR register :
 uint32_t RCC_GetPCLK1Value(void)
 {
     uint32_t pclk1,SystemClk ;
@@ -13,11 +14,11 @@ uint32_t RCC_GetPCLK1Value(void)
     clksrc = ((RCC->CFGR >> 2) & 0x3) ;
 
     if(clksrc == 0){
-        SystemClk = 16000000;
+        SystemClk = 16000000; //HSE
     }else if(clksrc ==1){
-        SystemClk = 8000000;
+        SystemClk = 8000000; //HSI
     }else if(clksrc ==2){
-        SystemClk = RCC_GetPLLValue(); 
+        SystemClk = RCC_GetPLLValue(); //PLL
     }
     return pclk1 ;
 }
@@ -30,5 +31,5 @@ void i2c_init(I2C_Handle_t *pI2CHandle)
 {
       uint32_t tempreg = 0;
       tempreg |= pI2CHandle->I2CConfig.I2C_ACKControl << 10 ;
-
+      
 }
